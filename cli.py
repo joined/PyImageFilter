212441781708d2eddc8515f9e9a0cfb63b005a72
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2015-02-04 15:22:16 +0100

    Fixins arguments order list.

--- cli.py
+++ cli.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     else:
         print('> Image file opened.')
 
-    ordered_args = args.order[9:-1]
+    ordered_args = args.order[11:-1]
 
     im_f = ImageFilter(im)
 
